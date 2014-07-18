a66231cf9fba4e45b373019e952d127f426785cb
James Wickett <wickett@gmail.com>
2014-07-18 10:07:33 -0500

    moving to slack_say lwrp syntax and added documentation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 slack CHANGELOG
 ===============
 
+0.1.1
+-----
+- [James Wickett] - bugfix Issue #1 and updated the LWRP to be slack_say 
+
 0.1.0
 -----
 - [Jason Rohwedder] - Initial release of slack
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jro@risk.io'
 license          'Apache 2.0'
 description      'slack LWRP for notifying slack.com channels'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 %w{redhat centos scientific fedora debian ubuntu arch freebsd amazon}.each do |os|
   supports os
