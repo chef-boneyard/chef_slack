745efad9ac0305350671d39dd09dff0d218f03f9
Tim Smith <tsmith84@gmail.com>
2015-10-31 19:29:33 -0700

    Update maintainer to be chef inc.

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'chef_slack'
-maintainer       'Ian Henry'
-maintainer_email 'ihenry@chef.io'
+maintainer       'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
 license          'Apache 2.0'
 description      'slack LWRP for notifying slack.com channels'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
@@ -9,3 +9,6 @@ version          '0.1.0'
 %w(redhat centos scientific fedora debian ubuntu arch freebsd amazon).each do |os|
   supports os
 end
+
+source_url 'https://github.com/chef-cookbooks/chef_slack' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/chef_slack/issues' if respond_to?(:issues_url)
