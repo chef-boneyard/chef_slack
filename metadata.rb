ac9f2af83556dde64b90ed5ee68ea4eecab2fc22
Tim Smith <tsmith@chef.io>
2016-06-30 10:32:24 -0700

    Testing updates
    Switch to cookstyle
    Properly fail in foodcritic
    Update test deps
    Run integration tests in travis
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -6,9 +6,11 @@ description      'slack LWRP for notifying slack.com channels'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-%w(redhat centos scientific fedora debian ubuntu arch freebsd amazon).each do |os|
+%w(ubuntu debian redhat centos suse opensuse opensuseleap scientific oracle amazon zlinux).each do |os|
   supports os
 end
 
 source_url 'https://github.com/chef-cookbooks/chef_slack' if respond_to?(:source_url)
 issues_url 'https://github.com/chef-cookbooks/chef_slack/issues' if respond_to?(:issues_url)
+
+chef_version '>= 11.0' if respond_to?(:chef_version)
