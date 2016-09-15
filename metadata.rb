a96dac997c74efcd040a565eccce79281b84f872
Tim Smith <tsmith@chef.io>
2016-09-15 16:47:39 -0700

    Require Chef 12.1
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -10,7 +10,7 @@ version          '0.2.0'
   supports os
 end
 
-source_url 'https://github.com/chef-cookbooks/chef_slack' if respond_to?(:source_url)
-issues_url 'https://github.com/chef-cookbooks/chef_slack/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/chef-cookbooks/chef_slack'
+issues_url 'https://github.com/chef-cookbooks/chef_slack/issues'
 
-chef_version '>= 11.0' if respond_to?(:chef_version)
+chef_version '>= 12.1'
