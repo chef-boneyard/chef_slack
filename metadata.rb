6f0c676027f02e80e45480210f5e9e8a51b08088
Tim Smith <tsmith84@gmail.com>
2019-10-07 15:10:34 -0700

    Fixes from Cookstyle 5.8
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,6 @@ maintainer       'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license          'Apache-2.0'
 description      'Slack resource for posting to slack.com channels'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '3.1.2'
 
 %w(ubuntu debian redhat centos suse opensuse opensuseleap scientific oracle amazon zlinux).each do |os|
@@ -12,4 +11,4 @@ end
 
 source_url 'https://github.com/chef-cookbooks/chef_slack'
 issues_url 'https://github.com/chef-cookbooks/chef_slack/issues'
-chef_version '>= 12.7' if respond_to?(:chef_version)
+chef_version '>= 12.7'
